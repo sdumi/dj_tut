5ba0ec34fdab30f18ddd44f5e8d0acfbb3bbcc07
Dumitru Sipos <dumitru.sipos@gmail.com>
2010-09-09 17:10:54 +0300

    used {% url %} to decouple the url from the actual server for example: links like: '/polls/{{ poll.id }}' did not work after moving the whole django projects under 'WWW.ROOTSITE.COM/django/' instead of 'WWW.ROOTSITE.COM/' as they originally were.
    this move was done due to using lighttpd in front of apache: lighttpd serving all the static data
    while apache serves only the dynamic requests (django among them)

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -6,8 +6,8 @@ info_dict = {
 }
 
 urlpatterns = patterns('',
-    (r'^$', 'django.views.generic.list_detail.object_list', info_dict),
-    (r'^(?P<object_id>\d+)/$', 'django.views.generic.list_detail.object_detail', info_dict),
-    (r'^(?P<object_id>\d+)/results/$', 'django.views.generic.list_detail.object_detail', dict(info_dict, template_name='polls/results.html'), 'poll_results'),
+    url(r'^$', 'django.views.generic.list_detail.object_list', info_dict, name='dum_polls'),
+    url(r'^(?P<object_id>\d+)/$', 'django.views.generic.list_detail.object_detail', info_dict, 'dum_poll_id'),
+    url(r'^(?P<object_id>\d+)/results/$', 'django.views.generic.list_detail.object_detail', dict(info_dict, template_name='polls/results.html'), 'poll_results'),
     (r'^(?P<object_id>\d+)/vote/$', 'mysite.polls.views.vote'),
 )
